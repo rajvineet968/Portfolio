0d65fc0af48ec8a0a1ccf1a185fa5202a74744f5
vineet_raj22 <rajvineet968@gmail.com>
2025-08-10 18:55:56 +0530

    Update index.js
    Updated

--- index.js
+++ index.js
@@ -11,22 +11,26 @@ app.set("views", path.join(__dirname, "views")); //ejs
 
 app.use(express.static(path.join(__dirname, "public"))); //css,html files
 
+app.get('/', (req, res) => {
+  res.redirect('/home');
+});
+
 app.get('/home',(req,res)=>{
     res.render("home.ejs");
-})
+});
 
 app.get('/home/aboutme',(req,res)=>{
     res.render("aboutme.ejs");
-})
+});
 
 app.get('/home/myprojects',(req,res)=>{
     res.render("myprojects.ejs");
-})
+});
 
 app.get('/home/miscellaneous',(req,res)=>{
     res.render("misc.ejs");
-})
+});
 
 app.get('/home/contactme',(req,res)=>{
     res.render("contactme.ejs");
-})
+});
